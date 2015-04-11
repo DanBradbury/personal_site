44e8f5aa4bd241f0b89a8b8c2d201ac7ab846b5b
Dan Bradbury <dan.luckydaisy@gmail.com>
2015-04-11 11:38:30 -0700

    heroku + rails out of the box = ?

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Danbradbury::Application.config.secret_token = '693cf114ac17ef8a7e91e0ad9d3fa5c1076c3741aff3932baea32e0ae7db6e5443127dd3eaa31b9034c656cd30a6696c437a7b9ca1a6a6172f9a1451ebb7aa46'
