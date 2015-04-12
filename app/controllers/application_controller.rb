d53130b9f5490d7d65e870256fc8a1d1aca38f27
Dan Bradbury <dan.luckydaisy@gmail.com>
2015-04-12 01:44:35 -0700

    add redcarpet before the nyquil kicks in..

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
+  helper :all
+  helper_method :md_render
   protect_from_forgery
+
+  def initialize(*args)
+    @renderer ||= Redcarpet::Render::HTML.new({})
+    @markdown_renderer ||= Redcarpet::Markdown.new(@renderer, {})
+  end
+
+  def md_render(text)
+    @markdown_renderer.render(text)
+  end
 end
